575a3f7656f969b2de69cc8c27b72db351d876ca
tcw0fs <andrii.s@radency.com>
2024-11-06 17:23:31 +0100

    refactor: rename project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# card-sdk
+# fools-money-core
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/tcw0fs/card-sdk)
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/tcw0fs/fools-money-core)
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     copyPublicDir: false,
-    lib: { entry: resolve(__dirname, 'lib/main.ts'), name: 'card-sdk' },
+    lib: { entry: resolve(__dirname, 'lib/main.ts'), name: 'fools-money-core' },
   },
 });
